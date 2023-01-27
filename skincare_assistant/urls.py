abc9a983ebdc0b67321704f2ed9063a09fc17daa
leszczynka <karolina.leszczynska05@gmail.com>
2023-01-27 12:39:24 +0100

    Create user registration functionality

--- skincare_assistant/urls.py
+++ skincare_assistant/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from skincare_assistant import settings
+from django.conf.urls.static import static
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     path('', include('skincare_app.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
