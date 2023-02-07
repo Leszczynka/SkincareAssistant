544ac2f0bce9d1b2b97f01ab3a9578c259b34ea5
leszczynka <karolina.leszczynska05@gmail.com>
2023-02-07 12:44:00 +0100

    Create search products functionality

--- skincare_assistant/settings.py
+++ skincare_assistant/settings.py
@@ -139,7 +139,8 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
-API_KEY = env('API_KEY')
+RAPID_API_KEY = env('RAPID_API_KEY')
+RAPID_API_HOST = env('RAPID_API_HOST')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
